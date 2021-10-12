030e8d52fdf3840aca6a8c8b3c817bd762f2003f
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-12 16:26:54 +0000

    final

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #   To check address: sudo i2cdetect -y 1
 
+# import smbus and sleep function
 import smbus
 from time import sleep
 
+# PCF8591 composite class (given)
 class PCF8591:
 
   def __init__(self,address):
@@ -23,23 +25,27 @@ class PCF8591:
       except Exception as e:
           print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
 
+# new Joystick component class 
 class Joystick:
 
-  def __init__(self,address):
-    self.joystick = PCF8591(address)
+  def __init__(self,address):         # extend via composition
+    self.joystick = PCF8591(address)  # create object from composite class
   
-  def getX(self):
+  def getX(self):                     # method using composite class 'read' method to read channel 0 of PCF
     return (self.joystick.read(0))
 
   def getY(self):
-    return (self.joystick.read(1))
+    return (self.joystick.read(1))    # method using composite class 'read' method to read channel 1 of PCF
 
-theJoystick = Joystick(0x48)
-#if __name__ == "__main__":
-try:
-  while True:
+
+
+theJoystick = Joystick(0x48)       # new object theJoystick from component Joystick class on specified channel
+
+try:                # exception handling
+  while True:       # continually print the getX and getY values every 100ms
     print('%3.f,  %3.f' % (theJoystick.getX(),theJoystick.getY()))
     sleep(0.1)
+
 except KeyboardInterrupt: # if user hits ctrl-C
   print('\nExiting')
 except Exception as e: # catch all other errors
