f28f67fb8de0a482efe592789a01cf34554be255
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-10-12 16:08:20 +0000

    d

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ class Joystick:
     self.joystick = PCF8591(address)
   
   def getX(self):
-    return str(self.joystick.read(0))
+    return (self.joystick.read(0))
 
   def getY(self):
-    return str(self.joystick.read(1))
+    return (self.joystick.read(1))
 
 theJoystick = Joystick(0x48)
 if __name__ == "__main__":
   try:
     while True:
-      print ("{:<s}, {:<s}".format(theJoystick.getX(),theJoystick.getY()))
+      print ("{:<d}, {:<d}".format(theJoystick.getX(),theJoystick.getY()))
       sleep(0.1)
   except KeyboardInterrupt: # if user hits ctrl-C
     print('\nExiting')
